15f438b976071d9d3d6800e5360bea9f5cc0bbf3
silc-o <bhussienaldondie@gmail.com>
2026-07-21 19:48:55 +0800

    Refactor Todo constructor to allow optional ID parameter

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,6 @@
 export default class Todo {
-  constructor(name, description, dueDate, priority = 'low', notes) {
-    this.id = crypto.randomUUID();
+  constructor(name, description, dueDate, priority = 'low', notes, id = crypto.randomUUID()) {
+    this.id = id;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;

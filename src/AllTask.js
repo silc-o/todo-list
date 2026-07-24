dceba2830d4b151b396a0c71e6381d0e6feb5152
silc-o <bhussienaldondie@gmail.com>
2026-07-24 18:59:23 +0800

    Refactor task loading and rendering; move createTaskCard and related functions to DOMManager for better structure

--- src/AllTask.js
+++ src/AllTask.js
@@ -1,60 +1,16 @@
 import { getAllTodos } from "./AppManager.js";
-
-function createTask(todo) {
-  const content = document.querySelector('#content');
-
-  const todoCard = document.createElement('div')
-  todoCard.classList.add('todo-card')
-
-  const todoCardLeft = document.createElement('div');
-  todoCardLeft.classList.add('todo-left');
-  const toggleBtn = document.createElement('button');
-  toggleBtn.classList.add('toggle-btn');
-  toggleBtn.setAttribute('aria-label', 'Mark as complete');
-
-  const titleContainer = document.createElement('div');
-  titleContainer.classList.add('title-container')
-
-  const todoTitle = document.createElement('div');
-  todoTitle.classList.add('todo-title');
-  todoTitle.textContent = todo.title;
-
-  const todoNotes = document.createElement('div');
-  todoNotes.textContent = todo.notes;
-  titleContainer.appendChild(todoTitle);
-  titleContainer.appendChild(todoNotes);
-
-  todoCardLeft.appendChild(toggleBtn);
-  todoCardLeft.appendChild(titleContainer);
-
-  const todoCardRight = document.createElement('div');
-  todoCardRight.classList.add('todo-right');
-  const dateTime = document.createElement('time');
-  dateTime.textContent = todo.dueDate;
-
-  const editBtn = document.createElement('button');
-  editBtn.classList.add('edit-btn')
-  editBtn.textContent = 'edit';
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.classList.add('delete-btn');
-  deleteBtn.textContent = 'delete';
-
-  todoCardRight.appendChild(dateTime);
-  todoCardRight.appendChild(editBtn);
-  todoCardRight.appendChild(deleteBtn);
-
-  todoCard.appendChild(todoCardLeft);
-  todoCard.appendChild(todoCardRight);
-
-  content.appendChild(todoCard);
-}
+import { createTaskCard, updateSectionTitle, noTaskOutput } from "./DOMManager";
 
 export function loadAllTask() {
-  const sectionTitle = document.querySelector('#section-title');
-  sectionTitle.textContent = "All Tasks";
+  updateSectionTitle("All Tasks");
+  const content = document.querySelector('#content')
+  content.innerHTML = "";
 
   const todos = getAllTodos();
 
-  todos.forEach(todo => createTask(todo));
+  if (todos.length !== 0) {
+    todos.forEach(todo => createTaskCard(todo));
+  } else {
+    noTaskOutput();
+  }
 }
---
+++ src/DOMManager.js
@@ -0,0 +1,61 @@
+const content = document.querySelector('#content');
+
+export function createTaskCard(todo) {
+  const todoCard = document.createElement('div')
+  todoCard.classList.add('todo-card')
+
+  const todoCardLeft = document.createElement('div');
+  todoCardLeft.classList.add('todo-left');
+  const toggleBtn = document.createElement('button');
+  toggleBtn.classList.add('toggle-btn');
+  toggleBtn.setAttribute('aria-label', 'Mark as complete');
+
+  const titleContainer = document.createElement('div');
+  titleContainer.classList.add('title-container')
+
+  const todoTitle = document.createElement('div');
+  todoTitle.classList.add('todo-title');
+  todoTitle.textContent = todo.title;
+
+  const todoNotes = document.createElement('div');
+  todoNotes.textContent = todo.notes;
+  titleContainer.appendChild(todoTitle);
+  titleContainer.appendChild(todoNotes);
+
+  todoCardLeft.appendChild(toggleBtn);
+  todoCardLeft.appendChild(titleContainer);
+
+  const todoCardRight = document.createElement('div');
+  todoCardRight.classList.add('todo-right');
+  const dateTime = document.createElement('time');
+  dateTime.textContent = todo.dueDate;
+
+  const editBtn = document.createElement('button');
+  editBtn.classList.add('edit-btn')
+  editBtn.textContent = 'edit';
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.textContent = 'delete';
+
+  todoCardRight.appendChild(dateTime);
+  todoCardRight.appendChild(editBtn);
+  todoCardRight.appendChild(deleteBtn);
+
+  todoCard.appendChild(todoCardLeft);
+  todoCard.appendChild(todoCardRight);
+
+  content.appendChild(todoCard);
+}
+
+export function updateSectionTitle(title) {
+  const sectionTitle = document.querySelector('#section-title');
+  sectionTitle.textContent = title;
+}
+
+export function noTaskOutput() {
+  const noTask = document.createElement('div');
+  noTask.classList.add('no-task');
+  noTask.textContent = 'Yay, No Tasks!';
+  content.appendChild(noTask);
+}
